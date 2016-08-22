1d6dfdad5ed42da52bf2687f11a4a165de8129dc
denysryzhykh <ryzhykh.denys@gmail.com>
2016-08-22 21:02:08 +0300

    My first commit

--- denis/src/main/java/Test.java
+++ denis/src/main/java/Test.java
@@ -2,7 +2,16 @@
  * Created by evgeniy on 22/08/16.
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void binar(int a){
+        int b;
+        String res = "";
+        while(a !=0){
+            b = a%2;
+            res = b + res;
+            a = a/2;
+        } System.out.print(res);
+    }
+    public static void main(String [] args) {
+        binar(64);
     }
 }
