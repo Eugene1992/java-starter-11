36b6088baaa2a850dc15cc2730d8d678bee3120c
denysryzhykh <ryzhykh.denys@gmail.com>
2016-08-24 18:37:46 +0300

    Binary with argument

---
+++ denis/src/main/java/InnerLoopHomework.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Supervisor on 24.08.2016.
+ */
+public class InnerLoopHomework {
+    public static void main(String[] args) {
+        String res = "";
+        for (int i = 0; i < 10; i++) {
+            res += "0";
+            System.out.println(res + " ");
+        }
+    }
+}
